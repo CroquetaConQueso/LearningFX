3179d096d8f761403976db0d2b64831be00b7c2f
CroquetaConQueso <lacio161@gmail.com>
2025-07-13 16:23:24 +0200

    Boton, circle, working, argumment ActionEvent

--- src/main/java/com/example1/Controller.java
+++ src/main/java/com/example1/Controller.java
@@ -1,18 +1,31 @@
 package com.example1;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
 
 public class Controller {
+
+    //Esta anotación lo que hace es pasar todos los valores que se encuentran en fxml a esta clase, permitiendo acceder a ellos
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
+
     public void upController(ActionEvent e){
         System.out.println("UP!");
+        myCircle.setCenterY(y-=10);
     }
     public void downController(ActionEvent e){
         System.out.println("DOWN!");
+        myCircle.setCenterY(y+=10);
     }
     public void rightController(ActionEvent e){
         System.out.println("RIGHT!");
+        myCircle.setCenterX(x+=10);
     }
     public void leftController(ActionEvent e){
         System.out.println("LEFT!");
+        myCircle.setCenterX(x-=10);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.example1 to javafx.fxml;
     exports com.example1;
