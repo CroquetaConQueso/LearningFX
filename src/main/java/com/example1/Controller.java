416c3633ba26dbeb1e15fc39ceba5a201b2502ba
CroquetaConQueso <lacio161@gmail.com>
2025-07-13 18:49:41 +0200

    Preparing the setup to have two scenes

--- src/main/java/com/example1/Controller.java
+++ src/main/java/com/example1/Controller.java
@@ -2,15 +2,22 @@ package com.example1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     //Esta anotación lo que hace es pasar todos los valores que se encuentran en fxml a esta clase, permitiendo acceder a ellos
     @FXML
     private Circle myCircle;
     private double x;
     private double y;
+
 
     public void upController(ActionEvent e){
         System.out.println("UP!");
