60a5cb13841c31281c25386e4d7bc280d9ac26f1
CroquetaConQueso <lacio161@gmail.com>
2025-07-15 11:40:55 +0200

    Loop working whenever changing images

--- src/main/java/com/example1/Scene2Controller.java
+++ src/main/java/com/example1/Scene2Controller.java
@@ -8,17 +8,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Scene2Controller {
 
-    @FXML
     // Tiene que tener el nombre del label encontrado en secondary.fxml
+    @FXML
     Label textScene2;
+    @FXML
+    ImageView vistaImage;
+    @FXML
+    Button buttonImage;
+
+    Image myImage = new Image(getClass().getResourceAsStream("/com/example1/Wendy1.jpg"));
+    Image myImage1 = new Image(getClass().getResourceAsStream("/com/example1/Wendy.jpg"));
+
+    private boolean showingFirst = true;
 
     public void displayName(String username) {
         textScene2.setText("Hello: " + username);
+    }
+
+    public void displayImage() {
+        if (showingFirst) {
+            vistaImage.setImage(myImage);
+        } else {
+            vistaImage.setImage(myImage1);
+        }
+        showingFirst = !showingFirst;
     }
 
     public void goToScene3(ActionEvent event) throws IOException {
